64bb69bb9e4427b59dd4ec7f4d31ab92977151fb
Charlie Bertrand <charlie.bertrand@live.com>
2021-01-30 16:40:58 +0100

    adding block table, model & controller

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,7 +1,9 @@
 class Lecture < ApplicationRecord
   belongs_to :user
+  CATEGORY = ["Art", "Developpement", "Gastronomie", "Langue", "Musique", "Photo & video", "Gaming", "Beauté", "Culture générale", "Nature"]
   has_many :schoolings, dependent: :destroy
   validates :title, :description, presence: true
-  validates :category, presence: true, inclusion: { in: ["Art", "Developpement", "Gastronomie", "Langue", "Musique", "Photo & video", "Gaming", "Beauté", "Culture générale", "Nature"] }
+  validates :category, presence: true, inclusion: { in: CATEGORY }
   has_one_attached :photo
+  has_many :blocks
 end
