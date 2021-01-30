3db3328561515f0c374c2eb6315bdba880bd32b3
Charlie Bertrand <charlie.bertrand@live.com>
2021-01-30 10:45:28 +0100

    model validation on Lecture & user

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,3 +1,6 @@
 class Lecture < ApplicationRecord
   belongs_to :user
+  validates :title, :description, presence: true
+  validates :category, presence: true, inclusion: { in: ["art", "developpement web", "gastronomie", "langue", "musique", "photo & video", "gaming", "beauté", "culture générale"] }
+  has_one :photo
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :lectures
+  validates :role, presence: true, inclusion: { in: ["student", "teacher"] }
+  validates :first_name, :last_name, presence: true
 end
