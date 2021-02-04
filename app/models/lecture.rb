d61f03f7cfc95c23e9924400cdb654e9772697fd
amelien delahaie <amelien.delahaie@gmail.com>
2021-02-04 21:31:45 +0100

    dashboard jour3

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -2,6 +2,7 @@ class Lecture < ApplicationRecord
   belongs_to :user
   CATEGORY = ["Art", "Developpement", "Gastronomie", "Langue", "Musique", "Photo & video", "Gaming", "Beauté", "Culture générale", "Nature"]
   has_many :schoolings, dependent: :destroy
+  has_many :students, through: :schoolings, source: :user
   validates :title, :description, presence: true
   validates :category, presence: true, inclusion: { in: CATEGORY }
   has_one_attached :photo
