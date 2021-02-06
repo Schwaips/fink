4c6d4fc429daa984c9e44810f3db97511609e968
Charlie Bertrand <charlie.bertrand@live.com>
2021-02-06 17:51:23 +0100

    Adding JS to edit page

---
+++ app/javascript/components/edit-block.js
@@ -0,0 +1,27 @@
+
+// const editBlock = () => {
+//   const editButton = document.querySelectorAll(".edit-block");
+//   const formBlock = document.getElementById("form-for-edit");
+//   editButton.forEach((edit) => {
+//     edit.addEventListener("click", (event) => {
+//       event.preventDefault();
+//       const form = "<h1> Coucou </h1>" ;
+//       formBlock.insertAdjacentHTML("beforeend", form);
+//     })
+//   });
+// };
+
+// export { editBlock };
+
+
+
+
+
+
+
+
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'flashcards/block_id'
-  get 'flashcards/question'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -8,11 +6,8 @@ Rails.application.routes.draw do
     resources :schoolings, only: [:new, :create]
     resources :blocks, only:  [:create, :show, :edit, :new] do
       resources :flashcards, only:[:new, :create]
-  end
-
+    end
   resources :schoolings, only: [:index]
-
-  get '/dashboard', to: "pages#dashboard"
-
-end
+  end
+   get '/dashboard', to: "pages#dashboard"
 end
