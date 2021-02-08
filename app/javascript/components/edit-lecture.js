65cc6ea0dbe06c5888029485bf8627a4c8d0a90f
Charlie Bertrand <charlie.bertrand@live.com>
2021-02-08 22:10:35 +0100

    starting ajaxification of manage course

--- app/javascript/components/edit-lecture.js
+++ app/javascript/components/edit-lecture.js
@@ -1,3 +1,4 @@
+
 // const editLecture = () => {
 //   const editButton = document.querySelectorAll(".edit-lecture");
 //   const formBlock = document.getElementById("form-for-edit-lecture");
@@ -12,9 +13,22 @@
 
 // export { editLecture };
 
+$("#edit-lecture-link").on("click", function () {
 
+  //call ajax to show preview of the post in the preview container
+  var lecture_id = $(this).data("lecture_id");
+  //this is the show call to lectures_controller#edit
+  $.ajax({
+    url: "/lectures/'" + lecture_id,
+    beforeSend: function (xhr) {
+      //change text of a tag to loading
+    }
+  })
+    .done(function (data) {
 
+    });
 
+})
 
 
 
