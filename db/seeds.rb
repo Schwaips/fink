55f1da560bcad33c92ef0210f87ab08003b85954
Charlie Bertrand <charlie.bertrand@live.com>
2021-02-25 19:32:43 +0100

    adding teacher to seed for content

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require "open-uri"
 
-puts "--- Destroying current data user/lecture ---"
+# puts "--- Destroying current data user/lecture ---"
 # User.destroy_all
 # Lecture.destroy_all
 puts "--starting seedings"
@@ -16,11 +16,11 @@ puts "--starting seedings"
 
 # CREATION TEACHER ET STUDENT INTERNE
 # puts "--creating teachers--"
-#   c = User.create!(first_name: "Charlie", last_name: "Bertrand", email: "charlie.bertrand@live.com", password: "123456", role: "teacher", slack_workspace_uid:"U01MY6XLZ29")
+#   c = User.create!(first_name: "Charlie", last_name: "Bertrand", email: "charlie.bertrand@live.com", password: "123456", role: "teacher", slack_workspace_uid:"U01MY6XLZ29", uid:"U01MY6XLZ29", provider: "slack")
 #   cb = URI.open('https://media-exp1.licdn.com/dms/image/C5603AQFbywQG0KAjeQ/profile-displayphoto-shrink_800_800/0/1539070374037?e=1619049600&v=beta&t=sV2smVTKLvzWhXZpAi5Oj84xwLYQ3SJ29_qpcA1O6lA')
 #   c.photo.attach(io: cb, filename: 'charlie-avatar.jpg', content_type: 'image/jpg')
 
-#   a = User.create!(first_name: "Amelien", last_name: "Delahaie", email: "amelien.delahaie@live.com", password: "123456", role: "teacher", slack_workspace_uid: "U01MJNMJ97D")
+#   a = User.create!(first_name: "Amelien", last_name: "Delahaie", email: "amelien.delahaie@live.com", password: "123456", role: "teacher", slack_workspace_uid: "U01MJNMJ97D", provider: "slack", uid: "U01MJNMJ97D")
 #   am = URI.open('https://pbs.twimg.com/profile_images/1177317334849130496/3-AonVB8_400x400.jpg')
 #   a.photo.attach(io: am, filename: 'amelien-avatar.jpg', content_type: 'image/jpg')
 
@@ -31,14 +31,14 @@ puts "--starting seedings"
 #   ob = URI.open('https://media-exp1.licdn.com/dms/image/C4D03AQGQMlEDoj0jwA/profile-displayphoto-shrink_800_800/0/1524840699999?e=1619049600&v=beta&t=e_GlYITjGLOTB5KsbhekUHimPjJBHBL8mJWtuc1qnj0')
 #   o.photo.attach(io: ob, filename: "olivia-avatar", content_type: "image/jpg")
 
-#   r = User.create!(first_name: "Raphael", last_name: "Mosca", email: "raphael.mosca@live.com", password: "123456", role: "student", slack_workspace_uid:"U01MM1C7L81")
+#   r = User.create!(first_name: "Raphael", last_name: "Mosca", email: "raphael.mosca@live.com", password: "123456", role: "student", slack_workspace_uid:"U01MM1C7L81", uid="U01MM1C7L81", provider: "slack")
 #   rm = URI.open('https://media-exp1.licdn.com/dms/image/C4D03AQFyEtL7SCBiEQ/profile-displayphoto-shrink_800_800/0/1595506575911?e=1619049600&v=beta&t=pZRj6JLzijvGfXt3C3heMG04LuUyfiXgYsAthqPZvJI')
 #   r.photo.attach(io: rm, filename: "raph-avatar.jpg", content_type: "image/jpg")
 # puts "--student created--"
 
 # Creation fake teacher & student
-
-alice = User.create!(first_name: "Alice", last_name: "Dupont", email: "alice@mail.fr", password: "alice@mail.fr", role: "teacher", slack_workspace_uid:"")
+# charlie
+alice = User.create!(first_name: "Rindra", last_name: "Dupont", email: "rindra@mail.fr", password: "rindra@mail.fr", role: "teacher", slack_workspace_uid:"")
 alicepic = URI.open('https://images.unsplash.com/photo-1484755560615-a4c64e778a6c?ixlib=rb-1.2.1')
 alice.photo.attach(io: alicepic, filename: 'alice-avatar.jpg', content_type: 'image/jpg')
 
@@ -46,80 +46,112 @@ pascal = User.create!(first_name: "Pascal", last_name: "Fodimian", email: "pasca
 pascalpic = URI.open('https://avatars.githubusercontent.com/u/4704178?v=4')
 pascal.photo.attach(io: pascalpic, filename: 'pascal-avatar.jpg', content_type: 'image/jpg')
 
+# pour amélien
+
+audrey = User.create!(first_name: "Audrey", last_name: "De Marco Khenissi", email: "audrey@mail.fr", password: "audrey@mail.fr", role: "teacher", slack_workspace_uid:"")
+audreypic = URI.open('https://avatars0.githubusercontent.com/u/69974522?v=4')
+audrey.photo.attach(io: audreypic, filename: 'audrey-avatar.jpg', content_type: 'image/jpg')
+
+
+antoine = User.create!(first_name: "Antoine", last_name: "Ung Bao", email: "antoine@mail.fr", password: "antoine@mail.fr", role: "teacher", slack_workspace_uid:"")
+antoinepic = URI.open('https://avatars.githubusercontent.com/u/61555263?v=4')
+antoine.photo.attach(io: antoinepic, filename: 'antoine-avatar.jpg', content_type: 'image/jpg')
+
+# olivia
+lomig = User.create!(first_name: "Lomig", last_name: "Enfroy", email: "lomig@mail.fr", password: "lomig@mail.fr", role: "teacher", slack_workspace_uid:"")
+lomigpic = URI.open('https://avatars.githubusercontent.com/u/1598346?v=4')
+lomig.photo.attach(io: lomigpic, filename: 'lomig-avatar.jpg', content_type: 'image/jpg')
+
+myriam = User.create!(first_name: "Myriam", last_name: "Faucher-Pelletier", email: "myriam@mail.fr", password: "myriam@mail.fr", role: "teacher", slack_workspace_uid:"")
+myriampic = URI.open('https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1599302914/hvk3p5ve0nwjkjbknybb.jpg')
+myriam.photo.attach(io: myriampic, filename: 'myriam-avatar.jpg', content_type: 'image/jpg')
+
+
+# raph
+
+coline = User.create!(first_name: "Coline", last_name: "Juillerat", email: "coline@mail.fr", password: "coline@mail.fr", role: "teacher", slack_workspace_uid:"")
+colinepic = URI.open('https://avatars1.githubusercontent.com/u/68182973?v=4')
+coline.photo.attach(io: colinepic, filename: 'coline-avatar.jpg', content_type: 'image/jpg')
+
+damien = User.create!(first_name: "Damien", last_name: "Milon", email: "damien@mail.fr", password: "damien@mail.fr", role: "teacher", slack_workspace_uid:"")
+damienpic = URI.open('https://avatars1.githubusercontent.com/u/68182973?v=4')
+damien.photo.attach(io: damienpic, filename: 'damien-avatar.jpg', content_type: 'image/jpg')
+
+puts "-- End of teacher creation--"
 
 # lecture creation
 
-puts "-- lecture pour charlie--"
-lecture = Lecture.create!(title: "Developpement Web Ruby", description: "Apprenez à developper sur Ruby on Rails en moins de 6 mois. Je vous fournis tout plein de video de cours. En parralèle, je peux faire des 1to1 sur demandes", category: "Developpement", user: c)
-file = URI.open('https://www.ideematic.com/wp-content/uploads/2018/05/ruby-on-rails.png')
-lecture.photo.attach(io: file, filename: 'rails.png', content_type: 'image/png')
 
-  b = Block.create!(lecture_id: lecture.id, title:"Introduction à rails", block_type: "Video", content: "Découvrez le monde de Rails grâce à mon tutoriel de deux heures. Apprenez à faire des seeds " )
-  b1 = Block.create!(lecture_id: lecture.id, block_type: "Podcast", title:"Utilisation des controllers", content: "Je vous explique a travers ce podcast comment créer une app rails" )
+# lecture = Lecture.create!(title: "Developpement Web Ruby", description: "Apprenez à developper sur Ruby on Rails en moins de 6 mois. Je vous fournis tout plein de video de cours. En parralèle, je peux faire des 1to1 sur demandes", category: "Developpement", user: c)
+# file = URI.open('https://www.ideematic.com/wp-content/uploads/2018/05/ruby-on-rails.png')
+# lecture.photo.attach(io: file, filename: 'rails.png', content_type: 'image/png')
 
-
-lecture1 = Lecture.create!(title: "Peinture à l'huile", description: "Je vous accompagne pour vos projets de peinture à l'huile. Toutes les techiques nécéssaires à l'implémentation de vos peintures", category: "Art", user: c)
-file = URI.open('https://mi0.rightinthebox.com/images/x/201905/dveccr1557396967601.jpg')
-lecture1.photo.attach(io: file, filename: 'paint-oil.jpg', content_type: 'image/jpg')
-
-  b2 = Block.create!(lecture_id: lecture1.id, block_type: "Text", title:"Tricks pour peinture", content: "Voici un cheatsheet pour savoir comment faire une belle peinture à l'huile" )
-  b3 = Block.create!(lecture_id: lecture1.id, block_type: "Video", title:"Dragons en peinture", content: "Une démonstration, comment faire des dragons avec de la peinture à l'huile" )
-
-artcomt = Lecture.create!(title: "Histoire de l'Art comtemporain", description: "Toutes les racines de l'art comtemporain", category: "Art", user: c)
-file = URI.open('https://www.guide-artistique.com/images/header/art-contemporain.jpg')
-artcomt.photo.attach(io: file, filename: 'art-comtem.jpg', content_type: 'image/jpg')
-
-  bartcomt1 = Block.create!(lecture_id: artcomt.id, block_type: "Video", title:"Introduction", content: "Quels sont les impacts de l'art comtenporain sur notre société actuelle?" )
-  bartcomp2 = Block.create!(lecture_id: artcomt.id, block_type: "Video", title:"Les grands acteurs du XXeme siecle", content: "Un plongeon au coeur des grands noms de l'art qui ont faconnés notre mode de vie" )
-
-artegy = Lecture.create!(title: "L'Egypte antique et l'art", description: "Les egyptiens a l'époque des pharaons et leurs approches vis à vis de l'art", category: "Art", user: c)
-file = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/%C3%84gyptischer_Maler_um_1360_v._Chr._001.jpg/384px-%C3%84gyptischer_Maler_um_1360_v._Chr._001.jpg')
-artegy.photo.attach(io: file, filename: 'art-comtem.jpg', content_type: 'image/jpg')
-
-  bartegy = Block.create!(lecture_id: artegy.id, block_type: "Video", title:"Des pharaons à aujourd'hui", content: "Les pyramides, le Sphinx, les pharaons...l'art d'aimer l'art" )
-  bartegy2 = Block.create!(lecture_id: artegy.id, block_type: "Video", title:"De l'or et des esclaves", content: "L'art, raconté en histoire par les égyptiens antiques" )
+#   b = Block.create!(lecture_id: lecture.id, title:"Introduction à rails", block_type: "Video", content: "Découvrez le monde de Rails grâce à mon tutoriel de deux heures. Apprenez à faire des seeds " )
+#   b1 = Block.create!(lecture_id: lecture.id, block_type: "Podcast", title:"Utilisation des controllers", content: "Je vous explique a travers ce podcast comment créer une app rails" )
 
 
-lecture13 = Lecture.create!(title: "Cours d'espagnol", description: "Apprenez l'espagnol, et envolez vous pour Barcelone", category: "Langue", user: c)
-file = URI.open('https://www.managementdelaformation.fr/wp-content/uploads/2018/05/RHEXIS_Actu_Espagne.jpg')
-lecture13.photo.attach(io: file, filename: 'spanish-class.jpg', content_type: 'image/jpg')
+# lecture1 = Lecture.create!(title: "Peinture à l'huile", description: "Je vous accompagne pour vos projets de peinture à l'huile. Toutes les techiques nécéssaires à l'implémentation de vos peintures", category: "Art", user: c)
+# file = URI.open('https://mi0.rightinthebox.com/images/x/201905/dveccr1557396967601.jpg')
+# lecture1.photo.attach(io: file, filename: 'paint-oil.jpg', content_type: 'image/jpg')
 
-lecture14 = Lecture.create!(title: "Cours d'italien", description: "Venez apprendre l'italien avec Charlie le polyglotte", category: "Langue", user: c)
-file = URI.open('https://www.airtransat.com/getmedia/06aeacd6-0120-454c-906b-d324b859577a/italie-cinque-terre-italy-2050x1200.aspx?width=2050&height=1200&ext=.jpg')
-lecture14.photo.attach(io: file, filename: 'italian-class.jpg', content_type: 'image/jpg')
+#   b2 = Block.create!(lecture_id: lecture1.id, block_type: "Text", title:"Tricks pour peinture", content: "Voici un cheatsheet pour savoir comment faire une belle peinture à l'huile" )
+#   b3 = Block.create!(lecture_id: lecture1.id, block_type: "Video", title:"Dragons en peinture", content: "Une démonstration, comment faire des dragons avec de la peinture à l'huile" )
 
-lecture15 = Lecture.create!(title: "Cours de français", description: "Parlez français avec Charlie", category: "Langue", user: c)
-file = URI.open('https://www.touteleurope.eu/fileadmin/user_upload/France.png')
-lecture15.photo.attach(io: file, filename: 'french-class.jpg', content_type: 'image/jpg')
+# artcomt = Lecture.create!(title: "Histoire de l'Art comtemporain", description: "Toutes les racines de l'art comtemporain", category: "Art", user: c)
+# file = URI.open('https://www.guide-artistique.com/images/header/art-contemporain.jpg')
+# artcomt.photo.attach(io: file, filename: 'art-comtem.jpg', content_type: 'image/jpg')
+
+#   bartcomt1 = Block.create!(lecture_id: artcomt.id, block_type: "Video", title:"Introduction", content: "Quels sont les impacts de l'art comtenporain sur notre société actuelle?" )
+#   bartcomp2 = Block.create!(lecture_id: artcomt.id, block_type: "Video", title:"Les grands acteurs du XXeme siecle", content: "Un plongeon au coeur des grands noms de l'art qui ont faconnés notre mode de vie" )
+
+# artegy = Lecture.create!(title: "L'Egypte antique et l'art", description: "Les egyptiens a l'époque des pharaons et leurs approches vis à vis de l'art", category: "Art", user: c)
+# file = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/%C3%84gyptischer_Maler_um_1360_v._Chr._001.jpg/384px-%C3%84gyptischer_Maler_um_1360_v._Chr._001.jpg')
+# artegy.photo.attach(io: file, filename: 'art-comtem.jpg', content_type: 'image/jpg')
+
+#   bartegy = Block.create!(lecture_id: artegy.id, block_type: "Video", title:"Des pharaons à aujourd'hui", content: "Les pyramides, le Sphinx, les pharaons...l'art d'aimer l'art" )
+#   bartegy2 = Block.create!(lecture_id: artegy.id, block_type: "Video", title:"De l'or et des esclaves", content: "L'art, raconté en histoire par les égyptiens antiques" )
 
 
-puts "-- lecture pour amélien--"
+# lecture13 = Lecture.create!(title: "Cours d'espagnol", description: "Apprenez l'espagnol, et envolez vous pour Barcelone", category: "Langue", user: c)
+# file = URI.open('https://www.managementdelaformation.fr/wp-content/uploads/2018/05/RHEXIS_Actu_Espagne.jpg')
+# lecture13.photo.attach(io: file, filename: 'spanish-class.jpg', content_type: 'image/jpg')
 
-lecture8 = Lecture.create!(title: "Chants", description: "Je suis prêt à entrainer vos belles voix pour vos plus beaux concerts", category: "Art", user: a)
-file = URI.open('https://p5.storage.canalblog.com/57/71/1380236/106139385.jpg')
-lecture8.photo.attach(io: file, filename: 'sing.jpg', content_type: 'image/jpg')
+# lecture14 = Lecture.create!(title: "Cours d'italien", description: "Venez apprendre l'italien avec Charlie le polyglotte", category: "Langue", user: c)
+# file = URI.open('https://www.airtransat.com/getmedia/06aeacd6-0120-454c-906b-d324b859577a/italie-cinque-terre-italy-2050x1200.aspx?width=2050&height=1200&ext=.jpg')
+# lecture14.photo.attach(io: file, filename: 'italian-class.jpg', content_type: 'image/jpg')
 
-  b4 = Block.create!(lecture_id: lecture8.id, block_type: "Text", title:"Mes créations", content: "voyez une de mes création pour vous divertir et apprendre à écrire des chansons" )
-  b5 = Block.create!(lecture_id: lecture8.id, block_type: "Video", title:"Echauffement, les bases", content: "Entrainez votre thorax avant de faire un concert" )
+# lecture15 = Lecture.create!(title: "Cours de français", description: "Parlez français avec Charlie", category: "Langue", user: c)
+# file = URI.open('https://www.touteleurope.eu/fileadmin/user_upload/France.png')
+# lecture15.photo.attach(io: file, filename: 'french-class.jpg', content_type: 'image/jpg')
 
 
-lecture9 = Lecture.create!(title: "Cours d'anglais 101", description: "Accompagnement pour l'apprentissage d'anglais niveau débutant", category: "Langue", user: a)
-file = URI.open('https://www.alternance-professionnelle.fr/wp-content/uploads/2019/03/cours-anglais-intensif-300x209.jpg')
-lecture9.photo.attach(io: file, filename: 'english-class.jpg', content_type: 'image/jpg')
+# puts "-- lecture pour amélien--"
 
-  b6 = Block.create!(lecture_id: lecture9.id, block_type: "Video", title:"Les prononciations", content: "Tous sur les prononciations des UKs, mieux comprendre et avancer." )
-  b6 = Block.create!(lecture_id: lecture9.id, block_type: "Text", title:"Verbes irréguliers", content: "Tous vos verbes irrégulier, facile à apprendre" )
+# lecture8 = Lecture.create!(title: "Chants", description: "Je suis prêt à entrainer vos belles voix pour vos plus beaux concerts", category: "Art", user: a)
+# file = URI.open('https://p5.storage.canalblog.com/57/71/1380236/106139385.jpg')
+# lecture8.photo.attach(io: file, filename: 'sing.jpg', content_type: 'image/jpg')
 
-lecture10 = Lecture.create!(title: "Python", description: "Venez découvrir Python", category: "Developpement", user: a)
-file = URI.open('https://www.lobservateurdebeauvais.fr/wp-content/uploads/2020/09/python.jpg')
-lecture10.photo.attach(io: file, filename: 'sing.jpg', content_type: 'image/jpg')
+#   b4 = Block.create!(lecture_id: lecture8.id, block_type: "Text", title:"Mes créations", content: "voyez une de mes création pour vous divertir et apprendre à écrire des chansons" )
+#   b5 = Block.create!(lecture_id: lecture8.id, block_type: "Video", title:"Echauffement, les bases", content: "Entrainez votre thorax avant de faire un concert" )
 
-lecture11 = Lecture.create!(title: "Javascript", description: "Venez découvrir Javascript", category: "Developpement", user: a)
-file = URI.open('https://www.developpez.net/forums/attachments/p513560d1/a/a/a')
-lecture11.photo.attach(io: file, filename: 'sing.jpg', content_type: 'image/jpg')
 
-lecture12 = Lecture.create!(title: "SQL", description: "Venez découvrir SQL", category: "Developpement", user: a)
-file = URI.open('https://www.softfluent.fr/wp-content/uploads/2019/10/SQL.png')
-lecture12.photo.attach(io: file, filename: 'sing.jpg', content_type: 'image/jpg')
+# lecture9 = Lecture.create!(title: "Cours d'anglais 101", description: "Accompagnement pour l'apprentissage d'anglais niveau débutant", category: "Langue", user: a)
+# file = URI.open('https://www.alternance-professionnelle.fr/wp-content/uploads/2019/03/cours-anglais-intensif-300x209.jpg')
+# lecture9.photo.attach(io: file, filename: 'english-class.jpg', content_type: 'image/jpg')
+
+#   b6 = Block.create!(lecture_id: lecture9.id, block_type: "Video", title:"Les prononciations", content: "Tous sur les prononciations des UKs, mieux comprendre et avancer." )
+#   b6 = Block.create!(lecture_id: lecture9.id, block_type: "Text", title:"Verbes irréguliers", content: "Tous vos verbes irrégulier, facile à apprendre" )
+
+# lecture10 = Lecture.create!(title: "Python", description: "Venez découvrir Python", category: "Developpement", user: a)
+# file = URI.open('https://www.lobservateurdebeauvais.fr/wp-content/uploads/2020/09/python.jpg')
+# lecture10.photo.attach(io: file, filename: 'sing.jpg', content_type: 'image/jpg')
+
+# lecture11 = Lecture.create!(title: "Javascript", description: "Venez découvrir Javascript", category: "Developpement", user: a)
+# file = URI.open('https://www.developpez.net/forums/attachments/p513560d1/a/a/a')
+# lecture11.photo.attach(io: file, filename: 'sing.jpg', content_type: 'image/jpg')
+
+# lecture12 = Lecture.create!(title: "SQL", description: "Venez découvrir SQL", category: "Developpement", user: a)
+# file = URI.open('https://www.softfluent.fr/wp-content/uploads/2019/10/SQL.png')
+# lecture12.photo.attach(io: file, filename: 'sing.jpg', content_type: 'image/jpg')
 
 puts "---seeds ending---"
